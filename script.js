ead9ce597e7aaacdf6c6be6cc888667b73e6e89a
syeboah-mensah <syeboah-mensah@ancoris.com>
2023-07-20 10:08:46 +0100

    fixed auto poll

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
-function poll(fn, timeout, interval) {
-    var endTime = Number(new Date()) + (timeout || 2000);
-    interval = interval || 100;
+function fetchFromAPI() {
+    fetch("http://localhost:8000/")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const tableBody = document.getElementById("tableBody");
 
-    var checkCondition = function (resolve, reject) {
-        var ajax = fn();
+            const tableRow = document.getElementsByClassName("rowData");
+            if (tableRow.length > 0){
+                element.remove(tableRow);
+            }
+            
 
-        ajax.then(function (response) {
-            // If the condition is met, we're done!
-            if (response) {
-                resolve(response);
-            }
-            // If the condition isn't met but the timeout hasn't elapsed, go again
-            else if (Number(new Date()) < endTime) {
-                setTimeout(checkCondition, interval, resolve, reject);
-            }
-            // Didn't match and too much time, reject!
-            else {
-                reject(new Error("timed out for " + fn + ": " + arguments));
-            }
+            data.forEach((item) => {
+                const row = document.createElement("tr");
+                row.className = "rowData";
+
+                const idCell = document.createElement("td");
+                idCell.textContent = item.title;
+                row.appendChild(idCell);
+
+                const nameCell = document.createElement("td");
+                nameCell.textContent = item.bibliography_citation;
+                row.appendChild(nameCell);
+
+                tableBody.appendChild(row);
+            });
+        })
+        .catch(function (error) {
+            console.log("Error fetching data:", error);
         });
-    };
-
-    return new Promise(checkCondition);
 }
 
-// Usage: get something via ajax
-poll(
-    function () {
-        return fetch("http://localhost:8000/");
-    },
-    2000,
-    10
-)
-    .then((response) => response.json())
-    .then((data) => {
-        const tableBody = document.getElementById("tableBody");
-
-        data.forEach((item) => {
-            const row = document.createElement("tr");
-
-            const idCell = document.createElement('td');
-            idCell.textContent = item.title;
-            row.appendChild(idCell);
-  
-            const nameCell = document.createElement('td');
-            nameCell.textContent = item.bibliography_citation;
-            row.appendChild(nameCell);
-
-            tableBody.appendChild(row);
-        });
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+fetchFromAPI();
+setInterval(fetchFromAPI, 10000);
